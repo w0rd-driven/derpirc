39edc55838a38460c8f897f1d0e1b65513099a52
Jeremy Brayton <jeremy@braytonium.com>
2011-06-11 19:44:12 -0400

    Added MVVMLight Reference and skeleton MainPage view models

---
+++ source/derpirc.WP7/ViewModels/MentionsViewModel.cs
@@ -0,0 +1,29 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace derpirc.ViewModels
+{
+    public class MentionsViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the ChannelViewModel class.
+        /// </summary>
+        public MentionsViewModel()
+        {
+            if (IsInDesignMode)
+            {
+                // code runs in blend --> create design time data.
+            }
+            else
+            {
+                // code runs "for real": connect to service, etc...
+            }
+        }
+
+        public override void Cleanup()
+        {
+            // clean own resources if needed
+
+            base.Cleanup();
+        }
+    }
+}
