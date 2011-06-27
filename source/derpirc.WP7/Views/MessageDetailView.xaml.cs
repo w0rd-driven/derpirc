29622c083cf40ed5b5641a69b303dcc09a1830b9
Jeremy Brayton <jeremy@braytonium.com>
2011-06-27 11:17:28 -0400

    Added Refresh icon + animation from Temp1 to use sometime later and a MessageDetailView.xaml placeholder page to start getting into navigation

---
+++ source/derpirc.WP7/Views/MessageDetailView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Phone.Controls;
+
+namespace derpirc.Views
+{
+    /// <summary>
+    /// Description for MessageDetailView.
+    /// </summary>
+    public partial class MessageDetailView : PhoneApplicationPage
+    {
+        /// <summary>
+        /// Initializes a new instance of the MessageDetailView class.
+        /// </summary>
+        public MessageDetailView()
+        {
+            InitializeComponent();
+        }
+    }
+}
