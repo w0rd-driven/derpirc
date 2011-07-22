d2dffa023993b025322c7c98920e66e5a8ca79e1
Jeremy Brayton <jeremy@braytonium.com>
2011-07-22 13:57:55 -0400

    Added NeutralResourcesLanguage fallback which seems necessary but maybe only for foreign apps that use English

--- source/derpirc.WP7/Properties/AssemblyInfo.cs
+++ source/derpirc.WP7/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Resources;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("derpirc.WP7")]
@@ -6,3 +7,5 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("73cfd17e-1012-4c52-8abc-d8efee45e348")]
+
+[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
