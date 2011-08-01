c74610d0eb5c45df4c8eb92b77139417bada8dab
Jeremy Brayton <jeremy@braytonium.com>
2011-08-01 05:37:08 -0400

    Added DataTemplateSelector implementation

---
+++ source/derpirc.WP7/Helpers/DataTemplateSelector.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace derpirc.Helpers
+{
+    public abstract class DataTemplateSelector : ContentControl
+    {
+        public DataTemplateSelector()
+        {
+            // Set the ContentControl to stretch, otherwise everything is centered
+            this.HorizontalAlignment = System.Windows.HorizontalAlignment.Stretch;
+            this.HorizontalContentAlignment = System.Windows.HorizontalAlignment.Stretch;
+            this.VerticalAlignment = System.Windows.VerticalAlignment.Stretch;
+            this.VerticalContentAlignment = System.Windows.VerticalAlignment.Stretch;
+        }
+
+        public virtual DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            return null;
+        }
+
+        protected override void OnContentChanged(object oldContent, object newContent)
+        {
+            base.OnContentChanged(oldContent, newContent);
+
+            ContentTemplate = SelectTemplate(newContent, this);
+        }
+    }
+}
---
+++ source/derpirc.WP7/Helpers/MessageTemplateSelector.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+
+namespace derpirc.Helpers
+{
+    public class MessageTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate TemplateMyMessage
+        {
+            get;
+            set;
+        }
+
+        public DataTemplate TemplateTheirMessage
+        {
+            get;
+            set;
+        }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            var entity = item as Data.ChannelMessage;
+            if (entity != null)
+            {
+                if (entity.Source == "w0rd-driven")
+                    return TemplateMyMessage;
+                else
+                    return TemplateTheirMessage;
+            }
+
+            return base.SelectTemplate(item, container);
+        }
+    }
+}
