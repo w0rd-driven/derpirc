08875fb173e0aa1d0096ba7598a0c0770422ca81
Jeremy Brayton <jeremy@braytonium.com>
2011-08-04 12:15:47 -0400

    Added Watermark TextBox behavior but may need an alternate method due to weird behavior. This only trips "properly" during the LostFocus event and strangely Mentions and Messages seem to be tied together, i.e. trigger one page and the other follows?

---
+++ source/derpirc.WP7/Helpers/WatermarkBehavior.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Interactivity;
+using System.ComponentModel;
+
+namespace derpirc.Helpers
+{
+    public class WatermarkBehavior : Behavior<TextBox>
+    {
+        public static readonly DependencyProperty WatermarkProperty = DependencyProperty.Register(
+                   "Watermark",
+                   typeof(string),
+                   typeof(WatermarkBehavior),
+                   new PropertyMetadata("Enter text here ..."));
+
+        [Description("Gets or sets the watermark text")]
+        public string Watermark
+        {
+            get { return GetValue(WatermarkProperty) as string; }
+            set { SetValue(WatermarkProperty, value); }
+        }
+
+        private bool _hasWatermark;
+        private Brush _textBoxForeground;
+
+        public string Text { get; set; }
+        public Brush Foreground { get; set; }
+
+        protected override void OnAttached()
+        {
+            _textBoxForeground = AssociatedObject.Foreground;
+
+            base.OnAttached();
+            if (Watermark != null)
+                SetWatermarkText();
+            AssociatedObject.GotFocus += GotFocus;
+            AssociatedObject.LostFocus += LostFocus;
+        }
+
+        private void LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (AssociatedObject.Text.Length == 0)
+                if (Watermark != null)
+                    SetWatermarkText();
+        }
+
+        private void GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (_hasWatermark)
+                RemoveWatermarkText();
+        }
+
+        private void RemoveWatermarkText()
+        {
+            AssociatedObject.Foreground = _textBoxForeground;
+            AssociatedObject.Text = string.Empty;
+            _hasWatermark = false;
+        }
+
+        private void SetWatermarkText()
+        {
+            AssociatedObject.Foreground = Foreground;
+            AssociatedObject.Text = Watermark;
+            _hasWatermark = true;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.GotFocus -= GotFocus;
+            AssociatedObject.LostFocus -= LostFocus;
+        }
+    }
+}
