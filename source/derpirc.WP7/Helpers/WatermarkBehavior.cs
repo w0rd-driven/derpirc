87cb652ffb6883ccb67500843cce66e34f08ff5d
Jeremy Brayton <jeremy@braytonium.com>
2011-08-05 06:23:03 -0400

    Made TextBoxTickStyle but would eventually like a control to be able to set Left/Right tick location & look like the Facebook comment box with embedded Send button. mc:Ignorable="d" is your friend and should be in EVERY xaml file.

--- source/derpirc.WP7/Helpers/WatermarkBehavior.cs
+++ source/derpirc.WP7/Helpers/WatermarkBehavior.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Net;
+﻿using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Ink;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
 using System.Windows.Interactivity;
-using System.ComponentModel;
+using System.Windows.Media;
 
 namespace derpirc.Helpers
 {
