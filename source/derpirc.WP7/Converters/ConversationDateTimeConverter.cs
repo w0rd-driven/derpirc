d11539d0c4913b78612864d1ef4d7c30ca872a4b
Jeremy Brayton <jeremy@braytonium.com>
2011-08-15 20:05:51 -0400

    Added ConversationDateTimeConverter to default to the normal conversation behavior (I think). There are 3 distinct time formats, today, any message until the FirstDayOfWeek and anything beyond (I have no clue what really old messages look like yet)

---
+++ source/derpirc.WP7/Converters/ConversationDateTimeConverter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace derpirc.Converters
+{
+    public class ConversationDateTimeConverter : IValueConverter
+    {
+        private string _stringDateFormat;
+        private string _stringTimeFormat = "h:mm:sst";
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTime? dateTime;
+            if (value is DateTime)
+            {
+                dateTime = value as DateTime?;
+                //IFormatProvider format = culture.GetFormat(targetType) as IFormatProvider;
+                var format = culture.DateTimeFormat;
+                format.AMDesignator = format.AMDesignator.ToLower();
+                format.PMDesignator = format.PMDesignator.ToLower();
+                var firstDayOfWeek = StartOfWeek(format);
+                if (dateTime > firstDayOfWeek)
+                {
+                    if (dateTime < DateTime.Today)
+                        _stringDateFormat = "ddd, ";
+                }
+                else
+                    _stringDateFormat = "M/d, ";
+                if (parameter == null || !(parameter is string))
+                {
+                    return dateTime.Value.ToString(_stringDateFormat + _stringTimeFormat, format);
+                }
+                else
+                {
+                    var result = dateTime.Value.ToString(parameter as string, format);
+                    return result;
+                }
+            }
+            System.Diagnostics.Debug.WriteLine("Failed to convert DateTime to string, value is not a valid DateTime.");
+            return value;
+        }
+
+        DateTime StartOfWeek(DateTimeFormatInfo format)
+        {
+            var firstDayOfWeek = format.FirstDayOfWeek;
+            return DateTime.Today.AddDays(-(DateTime.Today.DayOfWeek - firstDayOfWeek));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
