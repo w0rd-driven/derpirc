b800df0a32cb2d41dbb2c2042091fab1fbec9cbc
Jeremy Brayton <jeremy@braytonium.com>
2011-10-09 05:56:39 -0400

    Using icon set from Templarian. Added AboutView placeholder. Found the exact placement in ConnectionView's TitlePanel to mimic a pivot. Wired up ConnectionView to fleshed out VM

--- source/derpirc.WP7.Pages/ViewModels/ConnectionViewModel.cs
+++ source/derpirc.WP7.Pages/ViewModels/ConnectionViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Windows;
 using System.Windows.Data;
@@ -33,13 +36,33 @@ namespace derpirc.ViewModels
             }
         }
 
-        RelayCommand _reconnectCommand;
-        public RelayCommand ReconnectCommand
+        RelayCommand<IList> _selectionChangedToCommand;
+        public RelayCommand<IList> SelectionChangedCommand
         {
             get
             {
-                return _reconnectCommand ?? (_reconnectCommand =
-                    new RelayCommand(() => this.Reconnect()));
+                return _selectionChangedToCommand ?? (_selectionChangedToCommand =
+                    new RelayCommand<IList>((items) => this.SelectionChanged(items)));
+            }
+        }
+
+        RelayCommand _selectCommand;
+        public RelayCommand SelectCommand
+        {
+            get
+            {
+                return _selectCommand ?? (_selectCommand =
+                    new RelayCommand(() => this.Select()));
+            }
+        }
+
+        RelayCommand _connectCommand;
+        public RelayCommand ConnectCommand
+        {
+            get
+            {
+                return _connectCommand ?? (_connectCommand =
+                    new RelayCommand(() => this.Connect(SelectedItems)));
             }
         }
 
@@ -49,7 +72,17 @@ namespace derpirc.ViewModels
             get
             {
                 return _disconnectCommand ?? (_disconnectCommand =
-                    new RelayCommand(() => this.Disconnect()));
+                    new RelayCommand(() => this.Disconnect(SelectedItems)));
+            }
+        }
+
+        RelayCommand _reconnectCommand;
+        public RelayCommand ReconnectCommand
+        {
+            get
+            {
+                return _reconnectCommand ?? (_reconnectCommand =
+                    new RelayCommand(() => this.Reconnect(SelectedItems)));
             }
         }
 
@@ -76,7 +109,38 @@ namespace derpirc.ViewModels
             }
         }
 
+        private ClientInfo _selectedItem;
+        public ClientInfo SelectedItem
+        {
+            get { return _selectedItem; }
+            set
+            {
+                if (_selectedItem == value)
+                    return;
+
+                _selectedItem = value;
+                RaisePropertyChanged(() => SelectedItem);
+            }
+        }
+
+        private bool _isSelectionEnabled;
+        public bool IsSelectionEnabled
+        {
+            get { return _isSelectionEnabled; }
+            set
+            {
+                if (_isSelectionEnabled == value)
+                    return;
+
+                _isSelectionEnabled = value;
+                RaisePropertyChanged(() => IsSelectionEnabled);
+            }
+        }
+
         #endregion
+
+        private object _threadLock = new object();
+        private BackgroundWorker _worker;
 
         /// <summary>
         /// Initializes a new instance of the ConnectionViewModel class.
@@ -116,10 +180,32 @@ namespace derpirc.ViewModels
             else
             {
                 // Code runs "for real": Connect to service, etc...
-                Load();
+                _worker = new BackgroundWorker();
+                _worker.DoWork += new DoWorkEventHandler(DeferStartupWork);
+
+                DeferStartup(null);
             }
 
             Connections = new CollectionViewSource() { Source = _connectionsList };
+        }
+
+        internal void DeferStartup(Action completed)
+        {
+            _worker.RunWorkerAsync(completed);
+        }
+
+        private void DeferStartupWork(object sender, DoWorkEventArgs e)
+        {
+            Action completed = e.Argument as Action;
+            lock (_threadLock)
+            {
+                Load();
+            }
+
+            if (completed != null)
+            {
+                completed();
+            }
         }
 
         private void OnNavigatedTo()
@@ -132,6 +218,22 @@ namespace derpirc.ViewModels
 
         }
 
+        private void SelectionChanged(IList items)
+        {
+            // Set SelectedItems
+            if (SelectedItems == null)
+                SelectedItems = new ObservableCollection<ClientInfo>();
+            SelectedItems.Clear();
+            var list = items.Cast<ClientInfo>().ToList();
+            if (list.Count > 0)
+            {
+                list.ForEach(item => SelectedItems.Add(item));
+                // Set SelectedItem
+                if (list[0] != null)
+                    SelectedItem = list[0];
+            }
+        }
+
         private void Load()
         {
             _connectionsList.Clear();
@@ -140,30 +242,43 @@ namespace derpirc.ViewModels
                 _connectionsList.Add(item.Info);
             }
 
-            SupervisorFacade.Default.ClientStatusChanged += (s, e) => 
+            SupervisorFacade.Default.ClientStatusChanged += new EventHandler<ClientStatusEventArgs>(Default_ClientStatusChanged);
+        }
+
+        private void Default_ClientStatusChanged(object sender, ClientStatusEventArgs e)
+        {
+            var foundClient = _connectionsList.FirstOrDefault(x => x.Id == e.Info.Id);
+            if (foundClient == null)
+                _connectionsList.Add(e.Info);
+            else
             {
-                var foundClient = _connectionsList.FirstOrDefault(x => x.Id == e.Info.Id);
-                if (foundClient == null)
-                    _connectionsList.Add(e.Info);
-                else
-                {
-                    foundClient = e.Info;
-                }
-                DispatcherHelper.CheckBeginInvokeOnUI(() =>
-                {
-                    Connections.View.Refresh();
-                });
-            };
+                foundClient = e.Info;
+            }
+
+            DispatcherHelper.CheckBeginInvokeOnUI(() =>
+            {
+                Connections.View.Refresh();
+            });
         }
 
-        private void Reconnect()
+        private void Select()
         {
-
+            IsSelectionEnabled = !IsSelectionEnabled;
         }
 
-        private void Disconnect()
+        private void Connect(ObservableCollection<ClientInfo> clients)
         {
+            SupervisorFacade.Default.Connect(clients);
+        }
 
+        private void Disconnect(ObservableCollection<ClientInfo> clients)
+        {
+            SupervisorFacade.Default.Disconnect(clients);
+        }
+
+        private void Reconnect(ObservableCollection<ClientInfo> clients)
+        {
+            SupervisorFacade.Default.Reconnect(clients);
         }
     }
 }
---
+++ source/derpirc.WP7.Pages/Views/AboutView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Phone.Controls;
+
+namespace derpirc.Views
+{
+    /// <summary>
+    /// Description for AboutView.
+    /// </summary>
+    public partial class AboutView : PhoneApplicationPage
+    {
+        /// <summary>
+        /// Initializes a new instance of the AboutView class.
+        /// </summary>
+        public AboutView()
+        {
+            InitializeComponent();
+        }
+    }
+}
