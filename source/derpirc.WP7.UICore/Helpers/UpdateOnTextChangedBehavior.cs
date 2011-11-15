85059e43562405d9e8257f045d292d02153dd69b
Jeremy Brayton <jeremy@braytonium.com>
2011-11-15 09:35:26 -0500

    Removing WatermarkBehavior as it's kind of redundant now that networks are shown as PageSubtitle. It also had too many bugs to wish to worry about.

---
+++ source/derpirc.WP7.UICore/Helpers/UpdateOnTextChangedBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace derpirc.Helpers
+{
+    public class UpdateOnTextChangedBehavior : Behavior<TextBox>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.TextChanged += TextChanged;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.TextChanged -= TextChanged;
+        }
+
+        private void TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var binding = AssociatedObject.GetBindingExpression(TextBox.TextProperty);
+            if (binding != null)
+                binding.UpdateSource();
+        }
+    }
+}
